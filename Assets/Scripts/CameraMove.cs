43ba49b6e9448da4ac3a9f7462a0dec555d8025b
veryReallyGoodCoder <6000176@sae.edu.za>
2024-09-19 21:11:24 +0200

    OutdoorsScene || 3rd Person Char added

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private float YMin = -50.0f;
+    [SerializeField] private float YMax = 50.0f;
+
+    public Transform lookAt;
+
+    public Transform Player;
+
+    public float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    public float sensivity = 4.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.lockCursor = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentX += Input.GetAxis("Mouse X") * sensivity * Time.deltaTime;
+        currentY += Input.GetAxis("Mouse Y") * sensivity * Time.deltaTime;
+
+        //Debug.Log(currentX);
+
+        currentY = Mathf.Clamp(currentY, YMin, YMax);
+
+        Vector3 Direction = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * Direction;
+        //transform.rotation = rotation;
+
+        transform.LookAt(lookAt.position);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController Controller;
+    Animator anim;
+
+    public float Speed;
+
+    public Transform Cam;
+
+    private float gravity = -9.8f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float Horizontal = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+        float Vertical = Input.GetAxis("Vertical")  * Speed * Time.deltaTime;
+
+        Vector3 Movement = Cam.transform.right * Horizontal + Cam.transform.forward * Vertical;
+        Movement.y = gravity;
+
+        if(Horizontal != 0 || Vertical != 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        Controller.Move(Movement);
+
+        if (Movement.magnitude != 0f)
+        {
+            transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * Cam.GetComponent<CameraMove>().sensivity * Time.deltaTime);
+
+
+            Quaternion CamRotation = Cam.rotation;
+            CamRotation.x = 0f;
+            CamRotation.z = 0f;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, CamRotation, 0.1f);
+        }
+
+        if (Controller.isGrounded)
+        {
+            gravity = -1;
+            Debug.Log("grounded");
+        }
+        else
+        {
+            gravity = -9.8f;
+            Debug.Log("not grounded");
+
+        }
+
+    }
+}
